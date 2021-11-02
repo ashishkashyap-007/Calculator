23e6f17cefcf2c0473c3ecf2f0c8a0b7de2fbe1c
ashishkashyap-007 <ashish.kashyap.in@gmail.com>
2021-11-02 18:25:27 +0530

    Updated JS file

---
+++ script.js
@@ -0,0 +1,11 @@
+function clearScreen() {
+  document.getElementById("result").value = "";
+}
+
+function liveScreen(value) {
+  let res = document.getElementById("result");
+  if(res.value == "undefined"){
+    res.value = "";
+  }
+  res.value += value;
+}
